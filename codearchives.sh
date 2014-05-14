12e058a5a7febb079b22d2a7c06bbc7ff66f3d28
Spirit <spirit@quaddicted.com>
2014-05-14 21:02:01 +0200

    updated readme

--- codearchives.sh
+++ codearchives.sh
@@ -1,15 +1,17 @@
-workingdir=$(pwd)
+scriptdir=$(pwd)
+workingdir=~/archives/
 
 # initial grabbing, simply errors if the directory exists so re-running on the same repo does not hurt
+cd "${workingdir}"
 while read repo
 do
 	git clone ${repo} # no quotes here, git needs to see both arguments separatedly ;)
-done < repos.git
+done < ${workingdir}/repos.git
 
 while read repo
 do
 	git svn clone ${repo} # no quotes here, git needs to see both arguments separatedly ;)
-done < repos.svn
+done < ${workingdir}/repos.svn
 
 # updating
 cd "${workingdir}"
